86406abefd04e79fe4a2a66026e975eeef083633
feelmyears <philipmeyers2017@u.northwestern.edu>
2017-04-17 21:45:13 -0500

    Broke out correcting into separate file, cleaned up other files

--- src/counting.rs
+++ src/counting.rs
@@ -1,9 +1,6 @@
 use std::io::{BufRead, BufReader, Read};
 use std::string::String;
 use std::collections::HashMap;
-// use std::env;
-// use std::fs::File;
-// use std::io::prelude::*;
 
 extern crate regex;
 use self::regex::Regex;
@@ -25,4 +22,52 @@ pub fn read_corpus<R: Read>(reader: R) -> WordCounts {
     }
 
 	return counts;
+}
+
+#[cfg(test)]
+mod read_corpus_tests {
+	use super::read_corpus;
+	use super::WordCounts;
+	use std::io::Cursor;
+
+
+    #[test]
+    fn read_nothing_test() {
+    	let expected = WordCounts::new();
+        assert_read(&expected, "");
+    }
+
+    #[test]
+    fn read_unique_test() {
+    	let input = "hello world, it's me! \n Mario";
+    	let mut expected = WordCounts::new();
+		
+		expected.insert("hello".to_string(), 1);
+		expected.insert("world".to_string(), 1);
+		expected.insert("it's".to_string(), 1);
+		expected.insert("me".to_string(), 1);
+		expected.insert("mario".to_string(), 1);
+
+        assert_read(&expected, input);
+    }
+
+    #[test]
+    fn read_multiples_test() {
+    	let input = "hello world, it's me! \n Mario \n Mario it's me!";
+    	let mut expected = WordCounts::new();
+		
+		expected.insert("hello".to_string(), 1);
+		expected.insert("world".to_string(), 1);
+		expected.insert("it's".to_string(), 2);
+		expected.insert("me".to_string(), 2);
+		expected.insert("mario".to_string(), 2);
+
+        assert_read(&expected, input);
+    }
+
+    fn assert_read(expected: &WordCounts, input: &str) {
+        let mock_read = Cursor::new(input);
+        let counts = read_corpus(mock_read);
+        assert_eq!(expected.to_owned(), counts);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::io::{BufRead,BufReader,Read,stdin,Write,stdout};
+use std::io::{BufRead,BufReader,Read,stdin};
 use std::string::String;
-use std::collections::{HashMap, HashSet};
+// use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
 
 mod counting;
 use counting::*;
+
+mod correcting;
+use correcting::{correction, edits1, edits2};
 
 fn main() {
     //for w in edits2("to") {
@@ -21,7 +23,7 @@ fn main() {
 	let ref path = args[1];
 
 	let file = File::open(path).unwrap();
-	let mut buf_reader = BufReader::new(file);
+	let buf_reader = BufReader::new(file);
 	let corpus = read_corpus(buf_reader);
 
 	for (word, ct) in corpus {
@@ -35,9 +37,6 @@ fn main() {
 
 }
 
-// type WordCounts = HashMap<String, usize>;
-type WordSet = HashSet<String>;
-
 /// Reads all lines of an input source into a vector
 fn read_lines<R: Read>(reader: R) -> Vec<String> {
     let mut input: Vec<String> = vec![];
@@ -48,162 +47,6 @@ fn read_lines<R: Read>(reader: R) -> Vec<String> {
     }
 
     return input;
-}
-
-fn word_probability(word: &str, counts: &WordCounts) -> f64 {
-	match counts.get(word) {
-		Some(&count) => (count as f64) / (counts.len() as f64),
-		_ => 0.
-	}
-}
-
-fn known(words: &WordSet, counts: &WordCounts) -> WordSet {
-	let mut known_words = WordSet::new();
-
-	for w in words {
-		if counts.contains_key(w) {
-			known_words.insert(w.to_string()); 
-		}
-	} 
-
-	return known_words;
-}
-
-struct WordSplits {
-    word1: String,
-    word2: String,
-}
-
-const LETTERS: &'static str = "abcdefghijklmnopqrstuvwxyz";
-fn splits(word: &str) -> Vec<WordSplits> {
-    let mut splits: Vec<WordSplits> = Vec::new();
-    for i in 0..word.chars().count()+1 {
-        let mut word1 = "".to_string();
-        let mut word2 = "".to_string();
-        for j in 0..word.chars().count() {
-            if j < i {word1 = word1 + word.chars().nth(j).unwrap().to_string().as_str();}
-            else {word2 = word2 + word.chars().nth(j).unwrap().to_string().as_str();}
-            //splits.append((word1, word2));       
-        }
-        let w = WordSplits { word1: word1.clone(), word2: word2.clone() };
-        splits.push(w);
-    }
-
-    return splits;
-}
-
-fn deletes(splits: &Vec<WordSplits>, bucket: &mut WordSet) {
-    for w in splits {
-        let mut w2 = w.word1.clone();
-        if !w.word2.is_empty() {
-            //let mut w2 = w.word1 + w.word2[1:]
-            for i in 1..w.word2.chars().count() {
-                w2 = w2 + w.word2.chars().nth(i).unwrap().to_string().as_str();
-            }
-            bucket.insert(w2);
-        }
-    }
-}
-
-fn transposes(splits: &Vec<WordSplits>, bucket: &mut WordSet) {
-    for w in splits {
-        if w.word2.chars().count()>1 {
-            let mut w2 = w.word1.clone();
-            w2 = w2 + w.word2.chars().nth(1).unwrap().to_string().as_str();
-            w2 = w2 + w.word2.chars().nth(0).unwrap().to_string().as_str();
-            for i in 2..w.word2.chars().count() {
-                w2 = w2 + w.word2.chars().nth(i).unwrap().to_string().as_str();
-            }
-            bucket.insert(w2);
-        }
-    }
-}
-
-fn replaces(splits: &Vec<WordSplits>, bucket: &mut WordSet) {
-    for w in splits { 
-        if !w.word2.is_empty() {
-            for c in LETTERS.chars() {
-                let mut w2 = w.word1.clone();
-                w2 = w2 + c.to_string().as_str();
-                for i in 1..w.word2.chars().count() {
-                    w2 = w2 + w.word2.chars().nth(i).unwrap().to_string().as_str();
-                }
-                bucket.insert(w2);
-            }   
-        }
-    }
-}
-
-fn inserts(splits: &Vec<WordSplits>, bucket: &mut WordSet) {
-    for w in splits {
-        if !w.word2.is_empty() {
-            for c in LETTERS.chars() {
-                let mut w2 = w.word1.clone();
-                w2 = w2 + c.to_string().as_str();
-                for i in 0..w.word2.chars().count() {
-                    w2 = w2 + w.word2.chars().nth(i).unwrap().to_string().as_str();
-                }
-                bucket.insert(w2);
-            }
-        }
-        else {
-            for c in LETTERS.chars() {
-                let mut w2 = w.word1.clone();
-                w2 = w2 + c.to_string().as_str();
-                bucket.insert(w2);
-            }
-        }
-    }
-}
-
-fn edits1(word: &str) -> WordSet {
-    let splits = splits(word);
-    let mut edits = WordSet::new();
-   
-   	deletes(&splits, &mut edits);
-   	transposes(&splits, &mut edits);
-   	replaces(&splits, &mut edits);
-   	inserts(&splits, &mut edits);    
-    
-    return edits;
-}
-
-fn edits2(word: &str) -> WordSet {
-	let mut edits = WordSet::new();
-	for e1 in edits1(word) {
-		for e2 in edits1(e1.as_str()) {edits.insert(e2);}
-	}
-	edits
-}
-
-fn candidates(word: String, counts: &WordCounts) -> WordSet {
-	let mut original_word = WordSet::new();
-	original_word.insert(word.clone());
-	let candidates = known(&original_word, counts);
-	if candidates.len() > 0 {
-		return candidates;
-	}
-	let edits1_words = edits1(&word);
-	let candidates = known(&edits1_words, counts);
-	if candidates.len() > 0 {return candidates;}
-	let edits2_words = edits2(&word);
-	let candidates = known(&edits2_words, counts);
-	if candidates.len() > 0 {
-	 	return candidates;
-	}
-	return original_word;
-}
-
-fn correction(word: &str, counts: &WordCounts) -> String {
-	let mut max: f64 = -1.;
-	let mut max_word = word.to_string();
-	for c in candidates(word.to_string(), counts) {
-		if word_probability(c.as_str(), counts) > max {
-			max = word_probability(c.as_str(), counts);
-			max_word = c.to_string().clone();
-		}
-	}
-	max_word.to_string()
 }
 
 #[cfg(test)]
